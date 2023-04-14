2f31bdaa916519f6823b08acbd278def5b608dbe
Juan Manuel Restrepo Galarcio <94997816+manuelRestrep0@users.noreply.github.com>
2023-04-14 10:32:36 -0500

    add spring security

---
+++ src/main/java/com/manuel/workshop/seguridad/BasicAuthConfiguration.java
@@ -0,0 +1,56 @@
+package com.manuel.workshop.seguridad;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class BasicAuthConfiguration {
+
+    @Value("${usuario}")
+    private String username;
+
+    @Value("${password}")
+    private String password;
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails user = User
+                .withUsername(username)
+                .password(passwordEncoder().encode(password))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().hasRole("ADMIN")
+                )
+                .httpBasic(Customizer.withDefaults());
+
+
+        /*httpSecurity.authorizeRequests()
+                .antMatchers("/api/v1/disponibles").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();*/
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(8);
+    }
+}
